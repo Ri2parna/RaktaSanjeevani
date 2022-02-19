7aab77068b06153d3b28de413549a27675331d21
Rituparna <ri2parna@outlook.com>
2022-02-19 11:39:51 +0530

    complete upgradation process

--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,10 @@
 export * from "./TextBubble";
 export * from "./GradientButton";
+export * from "./Container";
+export * from "./CustomButton";
+export * from "./SimpleCard";
+export * from "./UserCard";
 export { default as CustomTextInput } from "./CustomTextInput";
 export { default as SubTitle } from "./SubTitle";
 export { default as Title } from "./Title";
+export { default as Screen } from "./Screen";
